50f2324624a087a51092a5f487511dfb6897c27e
aiwakuwaku <parplay1872@gmail.com>
2026-03-06 22:46:51 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,7 @@
-const CACHE_NAME = 'decade-golf-v1';
-const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './icon.svg',
-  'https://unpkg.com/vue@3/dist/vue.global.prod.js',
-  'https://cdn.tailwindcss.com'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+const CACHE_NAME = 'decade-v13';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./index.html', './manifest.json', './icon.svg'])));
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
